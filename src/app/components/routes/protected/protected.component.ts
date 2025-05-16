d1a66259433bbe9b2d24aee77fde3d07416af5de
mcer23 <melissa23cer@gmail.com>
2025-05-16 16:10:46 -0700

    condicional encuesta

--- src/app/components/routes/protected/protected.component.ts
+++ src/app/components/routes/protected/protected.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { AppService } from '../../../services/app.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-protected',
   standalone: true,
+  imports: [CommonModule,FormsModule ],
   templateUrl: './protected.component.html',
   styleUrl: './protected.component.scss'
 })
 export class ProtectedComponent {
+  parejaOpcion: string='';
+  alergiaOpcion:string='';
+  necesidadOpcion:string='';
+
+ 
+ 
   constructor(private appService: AppService) {}
  }
