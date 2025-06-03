24247c669c8526506f0a9810af9874a8fa068e8e
mcer23 <melissa23cer@gmail.com>
2025-06-03 15:36:08 -0700

    idk avances?

--- src/app/components/routes/protected/protected.component.spec.ts
+++ src/app/components/routes/protected/protected.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProtectedComponent } from './protected.component';
+
 
 describe('ProtectedComponent', () => {
   let component: ProtectedComponent;
